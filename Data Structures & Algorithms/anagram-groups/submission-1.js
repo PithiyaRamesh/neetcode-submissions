37e0ec5e43109b8bafd5b12beea2fc7eed3c2a82
Pithiya Ramesh <pithiyaramesh001@gmail.com>
2026-07-19 18:03:06 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,50 @@
+class Solution {
+    /*
+    1. Count character repeatation in each and every word
+    2. find the structully equal maps and group them
+    */
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const anagramList = [];
+        const wordCountMap = this.getMapWithCharCount(strs);
+        for (let i = 0; i<strs.length; i++) {
+            const countMap = wordCountMap.get(i);
+            if (!countMap) continue;
+            const subList = [];
+            for (let entry of wordCountMap) {
+                if (this.isEqualMap(countMap, entry[1])) {
+                    subList.push(entry[1].get("word"));
+                    wordCountMap.delete(entry[0]);
+                }
+            }
+            anagramList.push(subList);
+        }
+        return anagramList;
+    }
+
+    isEqualMap(map1, map2) {
+        return (
+            map1.size === map2.size &&
+            map1.keys().every((k) => k === "word" || (map2.has(k) && map2.get(k) == map1.get(k)))
+        );
+    }
+    getMapWithCharCount(strs) {
+        const mapOfCountList = new Map();
+        for (let i = 0; i < strs.length; i++) {
+            mapOfCountList.set(i, this.getWordCharCountMap(strs[i]));
+        }
+        return mapOfCountList; 
+    }
+
+    getWordCharCountMap(word) {
+        const map = new Map();
+        for (let c of word) {
+            map.set(c, map.get(c) + 1 || 1);
+        }
+        map.set("word", word)
+        return map;
+    }
+}
